3dc475539af6c1180ec35fa543c794a7afc6b142
Osincev Pavel <pavel.osincev04@mail.ru>
2024-08-14 15:33:05 +0500

    Update dashboard proposal

--- service/dashboard_service.py
+++ service/dashboard_service.py
@@ -35,6 +35,7 @@ def get_advanced_dashboard(user, start_date: datetime.date, end_date: datetime.d
         'success': 0,
         'reject': 0,
         'archived': 0,
+        'development': 0,
     }
     
     for status in ['SUCCESS', 'REJECT', 'ARCHIVED']:
@@ -44,6 +45,9 @@ def get_advanced_dashboard(user, start_date: datetime.date, end_date: datetime.d
                                      .group_by(ProposalVerdicts.project_proposal))
 
         proposal_count_list_by_status[status.lower()] += proposal_count_by_status
+
+    proposal_count_list_by_status['development'] = proposal_count - proposal_count_list_by_status['success'] - proposal_count_list_by_status['reject']
+    - proposal_count_list_by_status['archived']
 
     project_count_list_by_status = {
         'initiation': 0,
